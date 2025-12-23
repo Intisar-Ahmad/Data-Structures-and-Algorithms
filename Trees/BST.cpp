c132c116561218a4afef59b3452be1be7cc2f424
Intisar-Ahmad <intisar12122000@gmail.com>
2025-12-23 21:17:55 +0500

    Deletion in BST

--- Trees/BST.cpp
+++ Trees/BST.cpp
@@ -53,25 +53,26 @@ void postOrder(Node* root){
     cout << root->data << " ";
 }
 
-// searching in BST
-void search(Node* root, int data,int count){
+// searching in BST recursively
+Node* search(Node* root, int data,int count){
     if(!root){
         cout << "Not found\n";
-        return;
+        return nullptr;
     }
     if(root->data == data){
         cout << "Found at "<< count << endl;
-        return;
+        return root;
     }
     else if(data < root->data && root->left){
-        search(root->left,data,count + 1);
+        return search(root->left,data,count + 1);
     }
     else{
-        search(root->right,data, count + 1);
+        return search(root->right,data, count + 1);
     }
 
 }
 
+// searching iteratively
 void searchIteratively(Node* root, int data){
     Node* temp = root;
     int count = 1;
@@ -91,6 +92,45 @@ void searchIteratively(Node* root, int data){
     cout << "Not found\n";
 }
 
+// Find minimum value node in a subtree
+Node* findMin(Node* root) {
+    while (root && root->left)
+        root = root->left;
+    return root;
+}
+
+// deleting from a BST
+Node* deleteNode(Node* root, int key) {
+
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    }
+    else {
+        // Node found
+        if (!root->left) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (!root->right) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // Two children
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+
+
 int main() {
     Node* root = new Node(5); 
     insertNode(root,2);
@@ -100,21 +140,27 @@ int main() {
     insertNode(root,6);
     insertNode(root,7);
 
-    cout << "in-order\n";
-    inOrder(root);// fun fact: if the in-order traversal of a BT is sorted in ascending order, it means that the BT is a BST
-    cout << endl;
-    cout << "pre-order\n";
-    preOrder(root);
-    cout << endl;
-    cout << "post-order\n";
-    postOrder(root);
-    cout << endl;
+    // cout << "in-order\n";
+    // inOrder(root);// fun fact: if the in-order traversal of a BT is sorted in ascending order, it means that the BT is a BST
+    // cout << endl;
+    // cout << "pre-order\n";
+    // preOrder(root);
+    // cout << endl;
+    // cout << "post-order\n";
+    // postOrder(root);
+    // cout << endl;
 
-    search(root,6,1);
-    search(root,10,1);
+    // search(root,6,1);
+    // search(root,10,1);
 
-    // searching iteratively
-    searchIteratively(root,6); 
-    searchIteratively(root,10); 
+    // // searching iteratively
+    // searchIteratively(root,6); 
+    // searchIteratively(root,10); 
+
+    inOrder(root);
+    cout << endl;
+    root = deleteNode(root,7);
+    inOrder(root);
+    cout << endl;
     return 0;
 }
