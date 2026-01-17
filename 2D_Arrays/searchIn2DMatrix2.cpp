cee624127d1e155fcc3b02b236b1dcc471ea3f2e
Intisar-Ahmad <intisar12122000@gmail.com>
2026-01-17 14:40:55 +0500

    Search in 2D Matrix II

---
+++ 2D_Arrays/searchIn2DMatrix2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// this time matrices are different
+//[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]] something like this
+
+  bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = 0;
+        int col = n-1;
+        while(row<m && col>=0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+
+    }
+
+int main() {
+ 
+    return 0;
+}
