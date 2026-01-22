f5c6980294e4170450858d0ce48695e90add81e5
Intisar-Ahmad <intisar12122000@gmail.com>
2026-01-22 20:39:50 +0500

    Check if Array is Sorted and Rotated

---
+++ Arrays/Cpp/checkIfArrayisSortedandRotated.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+  bool check(vector<int>& nums) {
+      int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+int main() {
+ 
+    return 0;
+}
