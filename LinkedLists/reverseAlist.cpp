94f2670849850155a85d069c8b8fcf4f8664136d
Intisar-Ahmad <intisar12122000@gmail.com>
2026-01-23 19:04:10 +0500

    Reverse a Linked List(Recursively)

--- LinkedLists/reverseAlist.cpp
+++ LinkedLists/reverseAlist.cpp
@@ -14,6 +14,27 @@ using namespace std;
       ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
 
+
+ ListNode* func(ListNode* curr,ListNode* prev,ListNode* next){
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        if(!curr) return prev;
+        next = curr->next;
+        return func(curr,prev,next);
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return head;
+        
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* next = curr->next;
+
+        return func(curr,prev,next);
+
+    }
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
