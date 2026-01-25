1ac68f2c0e27af6607177e437f631f92db8a3e8e
Intisar-Ahmad <intisar12122000@gmail.com>
2026-01-25 17:07:14 +0500

    RotateKelements.cpp

---
+++ Arrays/Cpp/rotateKelements.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//   void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         k = k%n;
+//         vector<int> el(nums.begin() + n - k,nums.end());
+
+//         for(int i = n-k-1;i>=0;i--){
+//             nums[i+k] = nums[i];
+//         }
+
+//         for(int i = 0;i<k;i++){
+//             nums[i] = el[i];
+//         }
+
+//     }
+
+
+
+
+int main() {
+ 
+    return 0;
+}
