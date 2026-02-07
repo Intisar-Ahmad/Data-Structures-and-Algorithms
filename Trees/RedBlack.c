6f599ac6d72a196a2d9280cd372ff1cbafb619f7
Intisar-Ahmad <intisar12122000@gmail.com>
2026-02-07 18:45:48 +0500

    Red Black Tree in C

--- Trees/RedBlack.c
+++ Trees/RedBlack.c
@@ -1,7 +1,121 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-int main(int argc,char **argv) {
- 
+enum Color { RED, BLACK };
+
+struct Node {
+    int data;
+    enum Color color;
+    struct Node *left, *right, *parent;
+};
+
+struct Node* createNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->color = RED; // new nodes are always red 
+    node->left = node->right = node->parent = NULL;
+    return node;
+}
+
+void leftRotate(struct Node** root, struct Node* x) {
+    struct Node* y = x->right;
+    x->right = y->left;
+    if (y->left != NULL) y->left->parent = x;
+    y->parent = x->parent;
+    if (x->parent == NULL) *root = y;
+    else if (x == x->parent->left) x->parent->left = y;
+    else x->parent->right = y;
+    y->left = x;
+    x->parent = y;
+}
+
+void rightRotate(struct Node** root, struct Node* y) {
+    struct Node* x = y->left;
+    y->left = x->right;
+    if (x->right != NULL) x->right->parent = y;
+    x->parent = y->parent;
+    if (y->parent == NULL) *root = x;
+    else if (y == y->parent->left) y->parent->left = x;
+    else y->parent->right = x;
+    x->right = y;
+    y->parent = x;
+}
+
+void fixViolation(struct Node** root, struct Node* z) {
+    while (z != *root && z->parent->color == RED) {
+        if (z->parent == z->parent->parent->left) {
+            struct Node* y = z->parent->parent->right; // Uncle
+            if (y != NULL && y->color == RED) {
+                // uncle is red
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                //  uncle is black 
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    leftRotate(root, z);
+                }
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                rightRotate(root, z->parent->parent);
+            }
+        } else {
+
+            struct Node* y = z->parent->parent->left;
+            if (y != NULL && y->color == RED) {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    rightRotate(root, z);
+                }
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                leftRotate(root, z->parent->parent);
+            }
+        }
+    }
+    (*root)->color = BLACK;
+}
+
+void insert(struct Node** root, int data) {
+    struct Node* z = createNode(data);
+    struct Node* y = NULL;
+    struct Node* x = *root;
+
+    while (x != NULL) {
+        y = x;
+        if (z->data < x->data) x = x->left;
+        else x = x->right;
+    }
+    z->parent = y;
+    if (y == NULL) *root = z;
+    else if (z->data < y->data) y->left = z;
+    else y->right = z;
+
+    fixViolation(root, z);
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d(%s) ", root->data, root->color == RED ? "R" : "B");
+    inorder(root->right);
+}
+
+int main() {
+    struct Node* root = NULL;
+    insert(&root, 10);
+    insert(&root, 20);
+    insert(&root, 30);
+    insert(&root, 15);
+
+    printf("Inorder Traversal of Created Tree:\n");
+    inorder(root);
     return 0;
 }
